f5d36384e05c06208441b9e189ea80a499e88035
ccsexyz <ccsexyz@hotmail.com>
2016-09-11 11:18:04 +0800

    support rate limit in proxy

--- go/proxy.go
+++ go/proxy.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"container/list"
 	"encoding/binary"
 	"fmt"
 	"io"
 	"net"
 	"os"
 	"strconv"
+	"time"
+)
+
+type rateInfo struct {
+	bytes int
+	ch    chan bool
+}
+
+var (
+	readLimitChan chan rateInfo
+	writLimitChan chan rateInfo
 )
 
 func main() {
@@ -15,11 +27,20 @@ func main() {
 	checkErr(err)
 	listener, err := net.ListenTCP("tcp", tcpAddr)
 	checkErr(err)
+
+	go limitRateRoutine("read", readLimitChan)
+	go limitRateRoutine("write", writLimitChan)
+
 	for {
 		conn, err := listener.Accept()
 		checkErr(err)
 		go handleClient(conn)
 	}
+}
+
+func init() {
+	readLimitChan = make(chan rateInfo, 16)
+	writLimitChan = make(chan rateInfo, 16)
 }
 
 func handleClient(conn net.Conn) {
@@ -59,9 +80,10 @@ func handleClient(conn net.Conn) {
 	queue2 := make(chan string, 4)
 	finished := make(chan bool, 2)
 
-	f := func(self net.Conn, selfQueue chan string, pairQueue chan string, finishQueue chan bool) {
+	f := func(self net.Conn, selfQueue chan string, pairQueue chan string, finishQueue chan bool, limit chan rateInfo) {
 		buf := make([]byte, 16384)
 		finish := make(chan bool)
+		pending := make(chan bool)
 
 		go func() {
 			defer func() {
@@ -83,26 +105,86 @@ func handleClient(conn net.Conn) {
 		}()
 
 		for {
-			fmt.Println("try to read sth")
 			len, err := self.Read(buf)
-			fmt.Println("read sth")
 
 			if err != nil {
 				finish <- true
 				return
 			} else {
+				limit <- rateInfo{len, pending}
+				<-pending
 				pairQueue <- string(buf[0:len])
 			}
 		}
 	}
 
-	go f(conn2, queue2, queue1, finished)
-	f(conn, queue1, queue2, finished)
+	go f(conn2, queue2, queue1, finished, writLimitChan)
+	f(conn, queue1, queue2, finished, readLimitChan)
+}
+
+func limitRateRoutine(prefix string, ch chan rateInfo) {
+	t := time.NewTicker(time.Second)
+	totalBytes := 0
+	bytesPerSecond := 0
+	waitedSeconds := 0
+	pendingInfoList := list.New()
+
+	var bytesLimit int
+	if len(os.Args) == 3 {
+		bytesLimit, _ = strconv.Atoi(os.Args[2])
+	} else {
+		bytesLimit = 0
+	}
+
+	do := func(info rateInfo) {
+		bytes := info.bytes
+		ch := info.ch
+		totalBytes += bytes
+		bytesPerSecond += bytes
+		ch <- true
+	}
+
+	for {
+		select {
+		case <-t.C:
+			fmt.Printf("%v speed: %v\n", prefix, bytesPerSecond)
+			// fmt.Printf("%v waited seconds: %v\n", prefix, waitedSeconds)
+			bytesPerSecond = 0
+			for {
+				e := pendingInfoList.Front()
+				if e != nil {
+					info := e.Value.(rateInfo)
+
+					if info.bytes+bytesPerSecond > waitedSeconds*bytesLimit+bytesLimit {
+						waitedSeconds++
+						break
+					} else {
+						waitedSeconds = 0
+					}
+
+					do(info)
+					pendingInfoList.Remove(e)
+					if bytesPerSecond > bytesLimit {
+						break
+					}
+				} else {
+					break
+				}
+			}
+		case info := <-ch:
+			if bytesLimit == 0 || bytesPerSecond+info.bytes < bytesLimit {
+				do(info)
+			} else {
+				// fmt.Println("pending ", info.bytes, " bytes")
+				pendingInfoList.PushBack(info)
+			}
+		}
+	}
 }
 
 func checkArgs() {
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "usage: proxy local_addr\n")
+	if len(os.Args) != 2 && len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr, "usage: proxy local_addr limit_rate\n")
 		os.Exit(1)
 	}
 }
